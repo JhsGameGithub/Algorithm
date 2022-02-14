150a3bea45524b74faa61db9e22bf113397aba8e
Jhs <zlwlwhs25@game.hoseo.edu>
2022-02-15 02:33:05 +0900

    마구간 정하기(수정)
    알고리즘 44번(수정)

--- Algorithm/Question/Solution44.cpp
+++ Algorithm/Question/Solution44.cpp
@@ -12,14 +12,40 @@ int main(int argc, char* argv[])
 
 	vector<int> x(n);
 
+
 	for (int i = 0; i < n; i++)
+	{
 		cin >> x[i];
+	}
 	
 	sort(x.begin(), x.end());
 
-	int last = x[0], tmp, index;
+	int left = x[0], mid, right = x[n - 1], answer = 0, count, last;
 
-	while()
+	while (right >= left)
+	{
+		mid = (left + right) / 2;
+
+		count = 1;
+		last = x[0];
+		for (int i = 1; i < n; i++)
+		{
+			if (x[i] - last >= mid)
+			{
+				count++;
+				last = x[i];
+			}
+		}
+		if (count < c)
+			right = mid - 1;
+		else if (count >= c)
+		{
+			left = mid + 1;
+			answer = mid;
+		}
+	}
+
+	cout << answer;
 
 	return 0;
 }
