ec05951875190c4b28d40a8df458b6924c63c825
Jhs <zlwlwhs25@game.hoseo.edu>
2022-02-18 10:22:18 +0900

    올바른 괄호(stack)
    알고리즘 54번

---
+++ Algorithm/Question/Solution54.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+//¿Ã¹Ù¸¥ °ýÈ£(stack)
+int main(int argc, char* argv[])
+{
+	char c[31];
+	stack<int> correct;
+	cin >> c;
+
+	for (int i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] == '(')
+			correct.push(1);
+		else if (!correct.empty())
+			correct.pop();
+		else 
+		{
+			cout << "NO";
+			return 0;
+		}
+	}
+
+	cout << "YES";
+	return 0;
+}
