e51c0ab98282acb4b1a0e863bf57b47380d84515
Jhs <zlwlwhs25@game.hoseo.edu>
2022-03-17 09:42:42 +0900

    동전교환
    알고리즘 97번

--- Algorithm/Question/Solution97.cpp
+++ Algorithm/Question/Solution97.cpp
@@ -7,7 +7,24 @@ int main(int argc, char* argv[])
 {
 	int n, m;
 
-	cin >> n >> m;
+	cin >> n;
+	vector<int> coin(n);
+	for (int i = 0; i < n; i++)
+		cin >> coin[i];
+	cin >> m;
+	//합계,동전갯수
+	vector<int> exchange(m + 1);
+	int tmp;
+	for (int i = 0; i < coin.size(); i++)
+	{
+		for (int j = coin[i]; j <= m; j++)
+		{
+			tmp = exchange[j - coin[i]] + 1;
+			exchange[j] = tmp < exchange[j] || exchange[j] == 0 ? tmp : exchange[j];
+		}
+	}
+	
+	cout << exchange[m];
 
 	return 0;
 }
